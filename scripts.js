438976116a1d0a1502f2fbd852ed5d4a16802279
baovinhktqd <baovinhktqd@gmail.com>
2020-03-11 23:13:04 +0700

    ok ok

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+const nav = document.querySelector('.header .nav');
+const hamburger = document.querySelector('.header .menu-hamburger');
+
+
+hamburger.addEventListener('click', function() {
+   nav.classList.toggle("active");
+}) 
